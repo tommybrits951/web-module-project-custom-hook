a05ef3b35bd445210a49c4faedf542288e70339e
tommy brits <tommybrits951@gmail.com>
2023-01-14 20:44:38 -0800

    finished

---
+++ frontend/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react"
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage("darkMode", initialValue);
+
+    return [darkMode, setDarkMode];
+} 
+
+
+export default useDarkMode
